cf8ad30b93efae28977db99f43c7bc504b8ea8da
Moniappdeveloper <royalnorwork@gmail.com>
2023-11-14 22:10:45 +0100

    changed

--- main.tf
+++ main.tf
@@ -1,5 +1,53 @@
-resource "azurerm_resource_group" "rg-1" {
+
+
+# Random String Resource
+resource "random_string" "myrandom" {
+  length = 6
+  upper = false 
+  special = false
+   
+}
+
+
+# Create Resource Group
+resource "azurerm_resource_group" "rg_indexweb" {
     name = var.rg_name
     location = var.rg_location
   
 }
+
+# Create Storage Account
+resource "azurerm_storage_account" "static_storage" {
+  name                     = "${var.sa_webindex_name}${random_string.random_string.result}"
+  resource_group_name      = azurerm_resource_group.rg_indexweb.name
+  location                 = azurerm_resource_group.rg_indexweb.location
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  enable_https_traffic_only = true
+
+  static_website {
+    index_document = var.index_document
+  }
+  }
+
+
+# Add a index.html file to the storage account
+resource "azurem_storage_blob" "index-html" {
+    name = var.index_document
+    storage_account_name = azurerm_storage_account.sa_webindex_name
+    storage_container_name = "$web"
+    type = "Block"
+    content_type = "text/html"
+    source_content = var.source_content
+
+  
+}
+
+
+
+output "primary_web_endpoint" {
+  value = azurerm_storage_account.static_storage.primary_web_endpoint 
+  
+}
+
--- variables.tf
+++ variables.tf
@@ -5,5 +5,26 @@ variable "rg_name" {
 
 variable "rg_location" {
     type = string
-    description = "Location ofthe resource group"
+    description = "Location of the resource group"
+}
+variable "sa_webindex_name" {
+    type = string
+    description = "The name of storage account "
+}
+
+variable "storage_account_kind" {
+  description = "Storage Account Kind"
+  type = string   
+}
+variable "index_document" {
+  description = "static website index document"
+  type = string   
+}
+variable "static_website_error_404_document" {
+  description = "static website error 404 document"
+  type = string   
+}
+variable "source_content" {
+  description = "static website source folder"
+  type = string  
 }
